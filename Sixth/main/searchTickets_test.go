a1ec1a66f4962495048193972aa142afa12b1dcb
yudinRazDvaTri <46875481+yudinRazDvaTri@users.noreply.github.com>
2019-07-03 01:37:52 +0300

    Update searchTickets_test.go

--- Sixth/main/searchTickets_test.go
+++ Sixth/main/searchTickets_test.go
@@ -1,28 +1,46 @@
 package main
 
-import "testing"
+import (
+	"path/filepath"
+	"runtime"
+	"testing"
+)
 
-type testpair struct {
+var (
+	_, b, _, _ = runtime.Caller(0)
+	basepath  = filepath.Dir(b) + "\\file.txt"
+)
+
+type testTicketNumber struct {
 	ticketNumber uint
-	isCorrect bool
+	isCorrect    bool
 }
 
-var testsMoscow = []testpair{
+var testsMoscow = []testTicketNumber{
 	{123123, true},
 	{321321, true},
 	{113456, false},
 	{234567, false},
 }
 
-var testsPetersburg = []testpair{
+var testsPetersburg = []testTicketNumber{
 	{111122, true},
 	{131133, false},
 	{123123, false},
 	{172222, true},
 }
 
+type testLuckyTickets struct {
+	path string
+	totalTickets, moscowTickets, petersburgTickets uint
+}
+
+var testsLuckyTickets = []testLuckyTickets{
+	{basepath, 2, 1, 1},
+}
+
 func TestSearchAllMoscow(t *testing.T) {
-	for _ , pair := range testsMoscow {
+	for _, pair := range testsMoscow {
 		v := searchAllMoscow(pair.ticketNumber)
 		if v != pair.isCorrect {
 			t.Error(
@@ -35,7 +53,7 @@ func TestSearchAllMoscow(t *testing.T) {
 }
 
 func TestSearchAllPetersburg(t *testing.T) {
-	for _ , pair := range testsPetersburg {
+	for _, pair := range testsPetersburg {
 		v := searchAllPetersburg(pair.ticketNumber)
 		if v != pair.isCorrect {
 			t.Error(
@@ -46,3 +64,17 @@ func TestSearchAllPetersburg(t *testing.T) {
 		}
 	}
 }
+
+func TestFindLuckyTickets(t *testing.T) {
+	for _, pair := range testsLuckyTickets {
+		t1, t2, t3 := FindLuckyTickets(pair.path)
+		if t1 != pair.moscowTickets || t2 != pair.petersburgTickets || t3 != pair.totalTickets{
+			t.Error(
+				"For", pair.path,
+				"expected", pair.moscowTickets, pair.petersburgTickets, pair.totalTickets,
+				"got", t1, t2, t3,
+			)
+		}
+	}
+}
+
