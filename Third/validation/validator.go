ee81a5bf05c82e67137a4c348cbe1ea500955f96
yudinRazDvaTri <46875481+yudinRazDvaTri@users.noreply.github.com>
2019-07-03 07:12:32 +0300

    Update validator.go

--- Third/validation/validator.go
+++ Third/validation/validator.go
@@ -11,62 +11,61 @@ func IsAnswerIncorrect() bool {
 	return isAnswerIncorrect
 }
 
-// Проверка корректности введённых значений
-func ValidateFloat(s string) (bool, float64){
+// Validation of numbers
+func ValidateFloat(s string) (float64, bool) {
 	number, err := strconv.ParseFloat(s, 64)
 	if err != nil {
 		fmt.Printf("'%s' is not a number\n", s)
-		return false, 0
+		return 0, false
 	} else if number <= 0 {
 		fmt.Println("You have to input non-negative numbers!")
-		return false, 0
+		return 0, false
 	} else {
-		return true, number
+		return number, true
 	}
 }
 
-// Проверка, может ли треугольник с введёнными сторонами существовать
-func TriangleExistsValidator(sides []float64) bool{
-	return sides[0] + sides[1] > sides[2] &&
-		sides[0] + sides[2] > sides[1] &&
-		sides[1] + sides[2] > sides[0]
+// Validation of the entered as a triangle sides
+func TriangleExistsValidator(sides []float64) bool {
+	return sides[0]+sides[1] > sides[2] &&
+		sides[0]+sides[2] > sides[1] &&
+		sides[1]+sides[2] > sides[0]
 }
 
-// Общая проверка треугольника
-func FinalTriangleValidator(triangleSlice []string) ([]bool, []float64) {
+func FinalTriangleValidator(splitSlice []string) ([]float64, []bool) {
 	checker := make([]bool, 4)
 	floats := make([]float64, 3)
 	var isWrongInput bool
 
-	if len(triangleSlice[0]) <= 0 {	// Имя треугольника не может быть пустым
+	if len(splitSlice[0]) <= 0 { // Name of the triangle can't be empty
 		checker[0] = false
 		fmt.Println("Name of triangle can't be empty")
 	} else {
 		checker[0] = true
 	}
 
-	for i, j := 1, 0; i < 4; i, j = i + 1, j + 1 {			// Проверка введённых значений сторон треугольника
-		checker[i], floats[j] = ValidateFloat(triangleSlice[i]) // Если хотя бы одно значение недопустимо -
-		if checker[i] == false {				// выход из валидатора и оповещение об ошибке
+	for i, j := 1, 0; i < 4; i, j = i+1, j+1 { // validation of the entered numbers
+		floats[j], checker[i] = ValidateFloat(splitSlice[i]) // If at least one value is unacceptable -
+		if checker[i] == false {                             // return from validator and notification about the error
 			isWrongInput = true
 		}
 	}
 
 	if isWrongInput {
-		return checker, floats
+		return floats, checker
 	}
 
 	if !TriangleExistsValidator(floats) {
-		for i, j := 1, 0; i < 4; i, j = i + 1, j + 1 {
+		for i, j := 1, 0; i < 4; i, j = i+1, j+1 {
 			checker[i], floats[j] = false, 0
 		}
 		fmt.Println("Such a triangle cannot exist")
 	}
 
-	return checker, floats
+	return floats, checker
 }
 
-//Проверка корректности ответа пользователя
+// Validation of user's answer
 func ValidateAnswer(answer string) bool {
 	isAnswerIncorrect = false
 	if answer == "y" || answer == "yes" {
